75c82ec7a3683825e94881658e299fcdb200726c
janjao937 <janjao937@gmail.com>
2024-09-16 17:58:34 +0700

    projectile server and client

---
+++ Assets/Scripts/Core/Player/ProjectileLauncher.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ProjectileLauncher : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject serverProjectilePrefab;
+    [SerializeField] private GameObject clientProjectilePrefab;
+    [SerializeField] private GameObject muzzleFlash;
+    [SerializeField] private Collider2D playerCollider;
+    [Header("Setting")]
+    [SerializeField] private float projectileSpeed = 30;
+    [SerializeField] private float fireRate = 0.75f;
+    [SerializeField] private float muzzleFlashDuration = 0.075f;
+    private bool isFire;
+    private float previousFireTime;
+    private float muzzleFlashTimer;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) return;
+
+        inputReader.PrimaryFireEvent += HandlePrimaryFire;
+
+    }
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner) return;
+        inputReader.PrimaryFireEvent -= HandlePrimaryFire;
+    }
+    private void HandlePrimaryFire(bool isFire)
+    {
+        this.isFire = isFire;
+    }
+    private void Update()
+    {
+        if (muzzleFlashTimer > 0)
+        {
+            muzzleFlashTimer -= Time.deltaTime;
+            if (muzzleFlashTimer <= 0)
+            {
+                muzzleFlash.SetActive(false);
+            }
+        }
+        if (!IsOwner) return;
+        if (!isFire) { return; }
+        if(Time.time<(1/fireRate+previousFireTime)){return;}
+        
+        PrimaryFireServerRpc(projectileSpawnPoint.position, projectileSpawnPoint.up);
+        SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+
+        previousFireTime=Time.time;
+
+    }
+
+    [ServerRpc]
+    private void PrimaryFireServerRpc(Vector2 spawnPos, Vector2 dir)
+    {
+        GameObject projectileInstance = Instantiate(serverProjectilePrefab, spawnPos, Quaternion.identity);
+        projectileInstance.transform.up = dir;
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.velocity = rb.transform.up * projectileSpeed;
+        }
+
+        SpawnDummyProjectileClientRpc(spawnPos, dir);
+    }
+    [ClientRpc]
+    private void SpawnDummyProjectileClientRpc(Vector2 spawnPos, Vector2 dir)
+    {
+        if (IsOwner) return;
+        SpawnDummyProjectile(spawnPos, dir);
+    }
+    private void SpawnDummyProjectile(Vector2 spawnPos, Vector2 dir)
+    {
+        muzzleFlash.SetActive(true);
+        muzzleFlashTimer = muzzleFlashDuration;
+        GameObject projectileInstance = Instantiate(clientProjectilePrefab, spawnPos, Quaternion.identity);
+        projectileInstance.transform.up = dir;
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.velocity = rb.transform.up * projectileSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/LifeTime.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTime : MonoBehaviour
+{
+    [SerializeField] private float lifeTime;
+
+    private void Start(){
+        Destroy(this.gameObject,lifeTime);
+    }
+ }
