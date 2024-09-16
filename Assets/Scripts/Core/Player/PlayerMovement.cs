39ef559b399b4f7189d7e62ee0df7006d2de68a9
janjao937 <janjao937@gmail.com>
2024-09-16 16:32:00 +0700

    playermovement

---
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform bodyTransform;
+    [SerializeField] private Rigidbody2D rb;
+    [Header("Setting")]
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float turningRate = 270f;
+
+    private Vector2 previousMovementInput;
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) return;
+        inputReader.MoveEvent += HandleMove;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner) return;
+        inputReader.MoveEvent -= HandleMove;
+    }
+
+    private void Update()
+    {
+        if (!IsOwner) return;
+        float zRotate = previousMovementInput.x * (-turningRate) * Time.deltaTime;
+        bodyTransform.Rotate(0f, 0f, zRotate);
+    }
+    private void FixedUpdate(){
+        if (!IsOwner) return;
+        rb.velocity = (Vector2)bodyTransform.up * previousMovementInput.y * movementSpeed;
+    }
+    private void HandleMove(Vector2 movementInput)
+    {
+        previousMovementInput = movementInput;
+    }
+
+
+}
