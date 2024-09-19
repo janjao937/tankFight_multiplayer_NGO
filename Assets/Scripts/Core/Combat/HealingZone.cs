5516e7258aa2ca585463e4237e95d9d4eceb68a3
janjao937 <janjao937@gmail.com>
2024-09-19 13:55:34 +0700

    healing zone setup

---
+++ Assets/Scripts/Core/Combat/HealingZone.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealingZone : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Image healPowerBar;
+    [Header("Setting")]
+    [SerializeField] private int maxHealPower = 30;
+    [SerializeField] private float healCooldown = 60f;
+    [SerializeField] private float healTickRate = 1f;
+    [SerializeField] private int coinsPerTick = 10;
+    [SerializeField] private int healthPerTick = 10;
+
+    private List<TankPlayer> playersInZone = new List<TankPlayer>();
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!IsServer) { return; }
+        if (!col.attachedRigidbody.TryGetComponent<TankPlayer>(out TankPlayer player)) { return; }
+        playersInZone.Add(player);
+        Debug.Log("Entered Healing Zone:"+player.PlayerName.Value);
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (!IsServer) { return; }
+        if (!col.attachedRigidbody.TryGetComponent<TankPlayer>(out TankPlayer player)) { return; }
+        playersInZone.Remove(player);
+        Debug.Log("Exit Healing Zone:"+player.PlayerName.Value);
+    }
+}
