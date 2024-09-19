a9e150acf6a4d0f50c155f85afec11a4dfa9f18e
janjao937 <janjao937@gmail.com>
2024-09-20 00:24:25 +0700

    UGS DedicateServer Wrapper

--- Assets/Scripts/Networking/Shared/ApplicationData.cs
+++ Assets/Scripts/Networking/Shared/ApplicationData.cs
@@ -1,18 +1,114 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
-public class ApplicationData : MonoBehaviour
+
+public class ApplicationData
 {
-    // Start is called before the first frame update
-    void Start()
+    Dictionary<string, Action<string>> m_CommandDictionary = new Dictionary<string, Action<string>>();
+
+    const string k_IPCmd = "ip";
+
+    const string k_PortCmd = "port";
+    const string k_QueryPortCmd = "queryPort";
+
+    public static string IP()
     {
-        
+        return PlayerPrefs.GetString(k_IPCmd);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static int Port()
     {
-        
+        return PlayerPrefs.GetInt(k_PortCmd);
     }
+
+    public static int QPort()
+    {
+        return PlayerPrefs.GetInt(k_QueryPortCmd);
+    }
+
+   
+    public ApplicationData() //Ensure this gets instantiated Early on
+    {
+        SetIP("127.0.0.1");
+        SetPort("7777");
+        SetQueryPort("7787");
+        m_CommandDictionary["-" + k_IPCmd] = SetIP;
+        m_CommandDictionary["-" + k_PortCmd] = SetPort;
+        m_CommandDictionary["-" + k_QueryPortCmd] = SetQueryPort;
+        ProcessCommandLinearguments(Environment.GetCommandLineArgs());
+    }
+
+    void ProcessCommandLinearguments(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("Launch Args: ");
+        for (var i = 0; i < args.Length; i++)
+        {
+            var arg = args[i];
+            var nextArg = "";
+            if (i + 1 < args.Length) // if we are evaluating the last item in the array, it must be a flag
+                nextArg = args[i + 1];
+
+            if (EvaluatedArgs(arg, nextArg))
+            {
+                sb.Append(arg);
+                sb.Append(" : ");
+                sb.AppendLine(nextArg);
+                i++;
+            }
+        }
+
+        Debug.Log(sb);
+    }
+
+
+    bool EvaluatedArgs(string arg, string nextArg)
+    {
+        if (!IsCommand(arg))
+            return false;
+        if (IsCommand(nextArg)) // If you have need for flags, make a separate dict for those.
+        {
+            return false;
+        }
+
+        m_CommandDictionary[arg].Invoke(nextArg);
+        return true;
+    }
+
+    void SetIP(string ipArgument)
+    {
+        PlayerPrefs.SetString(k_IPCmd, ipArgument);
+    }
+
+    void SetPort(string portArgument)
+    {
+        if (int.TryParse(portArgument, out int parsedPort))
+        {
+            PlayerPrefs.SetInt(k_PortCmd, parsedPort);
+        }
+        else
+        {
+            Debug.LogError($"{portArgument} does not contain a parseable port!");
+        }
+    }
+
+    void SetQueryPort(string qPortArgument)
+    {
+        if (int.TryParse(qPortArgument, out int parsedQPort))
+        {
+            PlayerPrefs.SetInt(k_QueryPortCmd, parsedQPort);
+        }
+        else
+        {
+            Debug.LogError($"{qPortArgument} does not contain a parseable query port!");
+        }
+    }
+
+    bool IsCommand(string arg)
+    {
+        return !string.IsNullOrEmpty(arg) && m_CommandDictionary.ContainsKey(arg) && arg.StartsWith("-");
+    }
+
 }
--- Assets/Scripts/Networking/Shared/GameData.cs
+++ Assets/Scripts/Networking/Shared/GameData.cs
@@ -1,8 +1,40 @@
+using System;
+
+public enum Map
+{
+  Default
+}
+
+public enum GameMode
+{
+  Default
+}
+
+public enum GameQueue
+{
+  Solo,
+  Team
+}
 
 [System.Serializable]
 public class UserData
 {
   public string UserName;
   public string UserAuthId;
-    
+  public GameInfo userGamePreferences;
+
 }
+
+[Serializable]
+public class GameInfo
+{
+    public Map map;
+    public GameMode gameMode;
+    public GameQueue gameQueue;
+
+    public string ToMultiplayQueue()
+    {
+        return "";
+    }
+}
+
