6f8180ae6c6ef4eb95438c05ab9010247c636169
janjao937 <janjao937@gmail.com>
2024-09-23 22:14:22 +0700

    fix error network variable to show lobby code Only host

--- Assets/Scripts/UI/GameHUD.cs
+++ Assets/Scripts/UI/GameHUD.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using Unity.Collections;
 using Unity.Netcode;
@@ -7,28 +8,37 @@ using UnityEngine;
 public class GameHUD : NetworkBehaviour
 {
     [SerializeField] private TMP_Text lobbyCodeText = default;
-    // private NetworkVariable<FixedString32Bytes> lobbyCode = new NetworkVariable<FixedString32Bytes>(""); //have error on this
+    // private NetworkVariable<FixedString32Bytes> lobbyCode = new NetworkVariable<FixedString32Bytes>(""); //have error on this |wait to fix |update lobbyCode on network
+    private string hostLobbyCode = "";
+
   
     public override void OnNetworkSpawn()
     {
         if (IsClient)
         {
-            // lobbyCode.OnValueChanged += HandleLobbyCodeChange;
-            // HandleLobbyCodeChange(string.Empty, lobbyCode.Value);
+            // lobbyCode.OnValueChanged += HandleLobbyCodeChange;//wait to fix      | update lobbyCode on network
+            // HandleLobbyCodeChange(string.Empty, lobbyCode.Value);//wait to fix   | update lobbyCode on network
         }
         if (!IsHost) return;
-        
-        // lobbyCode.Value = HostSingleton.Instance.HostGameManager.JoinCode;
+        // lobbyCode.Value = HostSingleton.Instance.HostGameManager.JoinCode;//wait to fix | update lobbyCode on network
+
+        hostLobbyCode = HostSingleton.Instance.HostGameManager.JoinCode;
+        UpdateLobbyCodeUI(hostLobbyCode);
     }
     public override void OnNetworkDespawn()
     {
         if (IsClient)
         {
-            // lobbyCode.OnValueChanged -= HandleLobbyCodeChange;
+            // lobbyCode.OnValueChanged -= HandleLobbyCodeChange;//wait to fix | update lobbyCode on network
         }
     }
 
     private void HandleLobbyCodeChange(FixedString32Bytes oldCode, FixedString32Bytes newCode)
+    {
+        lobbyCodeText.text = newCode.ToString();
+    }
+
+    private void UpdateLobbyCodeUI(string newCode)
     {
         lobbyCodeText.text = newCode.ToString();
     }
