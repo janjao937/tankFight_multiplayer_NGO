6cd83145e0b436e315f8e99f226c92ab1cc5a2ff
janjao937 <janjao937@gmail.com>
2024-09-23 12:52:16 +0700

    comment error

--- Assets/Scripts/UI/GameHUD.cs
+++ Assets/Scripts/UI/GameHUD.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class GameHUD : NetworkBehaviour
 {
     [SerializeField] private TMP_Text lobbyCodeText = default;
-    // private NetworkVariable<FixedString32Bytes> lobbyCode = new NetworkVariable<FixedString32Bytes>("");
+    // private NetworkVariable<FixedString32Bytes> lobbyCode = new NetworkVariable<FixedString32Bytes>(""); //have error on this
 
     public override void OnNetworkSpawn()
     {
